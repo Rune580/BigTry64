227d1c2a3878bf3f7304d46ac497c2a9a41213bb
Rune <runemaster580@gmail.com>
2019-06-24 17:49:12 -0400

    had a stroke for an hour, finally got something started

---
+++ Imaging.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace BigTry64
+{
+    class Class1
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace BigTry64
 {
@@ -6,7 +11,57 @@ namespace BigTry64
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region startup
+            DiscordSocketClient Client;
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info
+            });
+
+            CommandService Commands;
+            Commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = true,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+            });
+            Client.Log += Log;
+            Task Log(LogMessage message)
+            {
+                Console.WriteLine(message.ToString());
+                return Task.CompletedTask;
+            }
+            Client.SetGameAsync($"image time");
+            //string Token = System.IO.File.ReadAllText("token.txt");
+            string Token;
+            using (StreamReader sr = new StreamReader("token.txt"))
+            {
+                Token = sr.ReadToEnd();
+            }
+
+
+            Client.LoginAsync(TokenType.Bot, Token);
+            Client.StartAsync();
+
+
+
+
+
+
+            #endregion
+
+            async Task MessageReceived(SocketMessage message)
+            {
+
+            }
+
+
+            Client.MessageReceived += MessageReceived;
+
+            do
+            {
+
+            } while (true);
         }
     }
 }
