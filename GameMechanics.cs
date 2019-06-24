8fd5ad1ec84635b633604ebea55edffd69de3ec2
Zachary Jones <dwelxs2@gmail.com>
2019-06-24 18:35:03 -0400

    sugondeeznuts

---
+++ GameMechanics.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigTry64
+{
+    public class Controller
+    {
+        public List<Mob> Players;
+        public List<Mob> Mobs;
+        public List<World> Worlds;
+    }
+    public class World
+    {
+        public string Name;
+        public Block[,] Blocks;
+    }
+
+
+    public class BaseObject
+    {
+        //Image to use when representing here
+    }
+    public class BaseMob : BaseObject
+    {
+        public int X, Y;
+        public int Health;
+        public Inventory Items;
+        public string World;
+        public void Move(string _Direction, int _Count = 0)
+        {
+
+        }
+    }
+    public class Mob : BaseMob
+    {
+        public void AutoMove()
+        {
+
+        }
+    }
+    public class Player : BaseMob
+    {
+        public ulong UserID;
+    }
+    public class Inventory
+    {
+
+    }
+
+    public class Item : BaseObject
+    {
+
+    }
+    public class Block : BaseObject
+    {
+        public bool ReadsItsText;
+        public string Text;
+        public bool Solid;
+    }
+}
